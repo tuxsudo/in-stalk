d6ebd39a012949783ba55f4d5661724e6664558e
Jared Anderson <jared@tuxsudo.com>
2015-10-15 17:07:30 -0600

    2.0 Release
    Refactor of es6 source.
    Refactor build process
    Added vertical in/out direction notification.
    Brand Change

--- README.md
+++ README.md
@@ -3,60 +3,55 @@
 Fire events when elements enter and exit the view port.
 
 
-## Usage
+## Usage -- Source ES6 Module
+
+
+	import inStalk from 'in-stalk';
+
+	var img = document.querySelector('.my-element');
+
+	img.addEventListener('in-stalk.in', function(){
+	          img.classList.add('active');
+	      });
+
+	      img.addEventListener('in-stalk.out', function(){
+	          img.classList.remove('active');
+	      });
+
+	inStalk.add( img );
+
+
+
+
+## Usage (as a global var)
 
 
 ### Add listeners to viewport in/out.
 
-```
-// Grab an element
-var element = document.querySelector('.some-selector');
 
-// react when element enters viewport
-element.addEventListener('viewport:in', function() {
-    element.setAttribute('data-inviewport', 1);
-});
+	// Grab an element
+	var element = document.querySelector('.some-selector');
 
-// react when element leaves viewport
-element.addEventListener('viewport:out', function() {
-    element.setAttribute('data-inviewport', 0);
-});
-```
+	// react when element enters viewport
+	element.addEventListener('in-stalk.in', function() {
+	    element.setAttribute('data-inviewport', 1);
+	});
+
+	// react when element leaves viewport
+	element.addEventListener('in-stalk.out', function() {
+	    element.setAttribute('data-inviewport', 0);
+	});
+
 
 ### Start watching an element
 
-```
-viewportwatcher.add(element);
-```
+
+	inStalk.add(element);
+
 
 ### Stop watching an element
 
-```
-viewportwatcher.remove(element);
 
-```
+	inStalk.remove(element);
 
 
-## Common Pitfalls
-
-For accurate position detection on _page load_, attach the instantiation of elements to `window.onload`:
-
-
-```
-window.addEventListener('load', function() {
-    var element = document.querySelector('.some-selector');
-
-    // react when element enters viewport
-    element.addEventListener('viewport:in', function() {
-        element.setAttribute('data-inviewport', 1);
-        viewportwatcher.remove( element );
-    });
-
-    // watch the element
-    viewportwatcher.add(element);
-});
-```
-
-## Dependencies
-
-[Modern browsers](https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent#Browser_compatibility) and IE9+ with [Polyfill for CustomEvent constructor](https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill).
---
+++ index.js
@@ -0,0 +1,127 @@
+import emit from 'dom-emit/from';
+import isInView from './lib/in-view.js';
+
+
+let isWatching = false,
+
+	initialized = false,
+
+	// watched elements
+	watchedItems = [],
+
+	// standard events to listen for (at window level)
+	standardEvents = ['load', 'scroll', 'hashchange', 'touchend', 'resize'],
+
+	// watch (at window level) for these custom events that may also change if an element is in the view port
+	// typically events causing elements display to toggle.
+	customEvents = [],
+
+	// stop watching scroll and stuff if there are no elements to watch
+	registerWatchListChange = function() {
+
+		if(watchedItems.length > 0 && !isWatching) {
+			attach();
+		}
+
+		else if(watchedItems.length === 0 && isWatching) {
+			detach();
+		}
+
+	},
+
+	// add event listeners to the likes of window scroll
+	attach = function(){
+		isWatching = true;
+		standardEvents.concat(customEvents)
+			.forEach( ev  => window.addEventListener(ev, checkItems) );
+	},
+
+	// remove event listeners to the likes of window scroll
+	detach = function(){
+		isWatching = false;
+		standardEvents.concat(customEvents)
+			.forEach( ev => window.removeEventListener(ev, checkItems) );
+	},
+
+	// check / update the status of an element
+	checkItem = function(item) {
+		var inview = isInView( item.element );
+
+		if(item.status ==='in' && !inview ) {
+			emit(item.element, "in-stalk.out");
+			emit(item.element, item.element.getBoundingClientRect().bottom < 0 ? 'in-stalk.out.top' : 'in-stalk.out.bottom');
+			item.status = 'out';
+
+		} else if(item.status==='out' && inview ) {
+			emit(item.element, "in-stalk.in");
+			emit(item.element, item.element.getBoundingClientRect().top<0 ? 'in-stalk.in.top' : 'in-stalk.in.bottom');
+			item.status = 'in';
+
+		} else if(item.status==='new') {
+			item.status = inview ? 'in' : 'out';
+
+			if(inview) {
+				emit(item.element, "in-stalk.in");
+			}
+		}
+	},
+
+	// what to execute while scrolling / moving viewport location
+	checkItems = function() {
+		initialized = true;
+		watchedItems.forEach( checkItem );
+	};
+
+
+
+export default {
+
+	// add element to stalk list
+	add: function(element) {
+		var config = {
+			element: element,
+			status: 'new'
+		};
+
+		watchedItems.push(config);
+
+		if(initialized) {
+			checkItem( config );
+		}
+
+		registerWatchListChange();
+
+	},
+
+	// remove an element from stalk list
+	remove: function(element) {
+		var newlist = watchedItems.filter(item=>item.element!==element);
+
+		if(newlist.length !== watchedItems.length) {
+			watchedItems = newlist;
+			registerWatchListChange();
+			return true;
+		}
+
+		return false;
+
+	},
+
+	listenFor: function(eventName) {
+		detach();
+		customEvents = customEvents.filter(ev=>ev!==eventName).concat(eventName);
+		registerWatchListChange();
+	},
+
+	ignore: function(eventName) {
+		detach();
+		customEvents = customEvents.filter(ev=>ev!==eventName);
+		registerWatchListChange();
+	},
+
+	check: checkItems
+
+};
+
+
+
